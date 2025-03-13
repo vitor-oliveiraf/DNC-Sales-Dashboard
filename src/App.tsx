a45189a48818cf73d3a2b532b1459c8c7ef4fa78
Vitor de Oliveira <vitoro.fabiano@gmail.com>
2025-03-13 15:48:03 -0300

    feat: add react-router-dom and create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<>Login</>} />
+        <Route path="/cadastro" element={<>Cadastro</>} />
+        <Route path="/home" element={<>Home</>} />
+        <Route path="/perfil" element={<>Perfil</>} />
+        <Route path="/leads" element={<>Leads</>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
